bb333bada0e68a56dee979c674c7930704f6af72
root <root@localhost.localdomain>
2014-07-09 18:31:13 +0800

    local

--- routes/movie.js
+++ routes/movie.js
@@ -1,26 +1,43 @@
 var Movie = require('./../models/Movie.js');
 exports.movieAdd = function(req, res) {
+	var perpage = 10;
 	if(req.params.name){
 		// executing a query explicitly
-		var query = Movie.find({ name: '/'+req.params.name+'/i' },null, { skip: req.params.page });
-		var supplies = query.limit(10);
-		return res.render('main/movie/movie', {
-		title:req.params.name+'|电影|管理|moive.me',
-		label:'编辑电影:'+req.params.name,
-		movie:req.params.name,
-		supplies:supplies
+		var query = Movie.find({ name: req.params.name },null, { skip: req.params.page });
+		query.limit(perpage).exec(function(err,docs){
+			var supplies;
+			if(err)
+				console.log("something wrong")
+			else{
+				supplies = docs;
+				console.log(supplies);
+				return res.render('main/movie/movie', {
+				title:req.params.name+'|电影|管理|moive.me',
+				label:'编辑电影:'+req.params.name,
+				movie:req.params.name,
+				supplies:supplies
+				});
+			}
 		});
+		
 	} else {
 		// executing a query explicitly
 		//var query = Movie.find({ name: /snickers/i }, null, { skip: 1 });
 		var query = Movie.find(null, null, { skip: req.params.page });
-		var supplies = query.limit(10).exec();
-		console.log(supplies);
-		return res.render('main/movie/movie',{
-		title:'新增加|电影|管理|moive.me',
-		label:'新增加电影',
-		movie:false,
-		supplies:supplies
+		query.limit(perpage).exec(function(err,docs){
+			var supplies;
+			if(err)
+				console.log("something wrong")
+			else{
+				supplies = docs;
+				console.log(supplies);
+				return res.render('main/movie/movie',{
+				title:'新增加|电影|管理|moive.me',
+				label:'新增加电影',
+				movie:false,
+				supplies:supplies
+				});
+			}
 		});
 	}
 };
